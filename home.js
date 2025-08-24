3f65c821e0591844b171c435ba1b21ef9798bae7
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-08-24 16:49:53 +0600

    update reusable function

--- home.js
+++ home.js
@@ -1,27 +1,35 @@
-const fixedPin = 2004;
+
+// .......Reuseable function ........
+// ............. get innet text ..........
+function getInnerText(id) {
+    const Text = parseInt(document.getElementById(id).innerText);
+    return Text
+}
+/**
+ * // ............. set innet text ..........
+function setInnerText(id var) {
+    const setText = parseInt(document.getElementById(id).innerText) = var;
+return setText
+}
+ */
+
+
+// ------ get Input value -------
+function getInputValue(id) {
+    const Input = parseInt(document.getElementById(id).value);
+    return Input
+}
+
+// ------ get Input integar value -------
+function getInputIntValue(id) {
+    const IntValue = parseInt(document.getElementById(id).value);
+    return IntValue
+}
+
+
+// -------univarsal varialble ------
 const fixedaccountNumber = 12345678910;
-const totalBalence = parseInt(document.getElementById("total-balence").innerText)
-
-document.getElementById("add-money-btn").addEventListener("click", function (e) {
-    e.preventDefault()
-
-    const bankName = document.getElementById("bank-name").value;
-    const accountNumber = parseInt(document.getElementById("account-number").value);
-
-    const addAmount = parseInt(document.getElementById("add-amount").value);
-
-    const inputPin = parseInt(document.getElementById("pin").value);
-    if (accountNumber !== fixedaccountNumber) {
-        alert("Please Valid Account Number ")
-        return
-    };
-    if (inputPin !== fixedPin) {
-        alert("Please Valid Pin Number ")
-        return
-    };
-    let totalNewBalence = totalBalence + addAmount;
-    document.getElementById("total-balence").innerText = totalNewBalence;
-})
+const fixedPin = 2004;
 
 
 //  ----------- all card  btn toggle  --------
@@ -29,7 +37,6 @@ const addMoneySection = document.getElementById("add-money-status");
 const cashOutSection = document.getElementById("cash-out-status");
 const addMoneyBtn = document.getElementById("add-money-card-btn");
 const cashOutBtn = document.getElementById("cash-out-btn");
-
 
 addMoneyBtn.addEventListener("click", function (e) {
     e.preventDefault()
@@ -44,22 +51,36 @@ cashOutBtn.addEventListener("click", function (e) {
     cashOutSection.style.display = 'block'
 })
 
+// ...... feature button ........
+// -----add money button ---
+document.getElementById("add-money-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    const availableBalance = getInnerText("total-balance");
+    const bankName = getInputValue("bank-name");
+    const accountNumber = getInputIntValue("account-number");
+    const addAmount = getInputIntValue("add-amount");
+    const inputPin = getInputIntValue("pin");
 
-// ---------------- withdrow Money btn --------------
+    if (document.getElementById("account-number").value.length !== 11) {
+        alert("Please Input 11 Digits Account Number")
+        return
+    }
+    if (inputPin !== fixedPin) {
+        alert("Please Input 4 Digits Pin")
+        return
+    }
 
-
-
-const pinn = parseInt(document.getElementById("pin").value)
-
+    const newAvailableBalance = availableBalance + addAmount;
+    document.getElementById("total-balance").innerText = newAvailableBalance
+})
+// ............ withdrow btn features .........
 const withdrowBtn = document.getElementById("withdrow-money-btn")
-
 withdrowBtn.addEventListener("click", function (e) {
     e.preventDefault()
-
-    const agentNumber = parseInt(document.getElementById("agent-number").value)
-    const pinNumber = parseInt(document.getElementById("pin-number").value)
-
-    const withdrowAmmount = parseInt(document.getElementById("withdrow-amount").value)
+    const availableBalance = getInnerText("total-balance");
+    const agentNumber = getInputIntValue("agent-number");
+    const pinNumber = getInputIntValue("pin-number");
+    const withdrowAmmount = getInputIntValue("withdrow-amount");
 
     if (document.getElementById("agent-number").value.length !== 11) {
         alert("Please input valid Agent Number")
@@ -70,7 +91,6 @@ withdrowBtn.addEventListener("click", function (e) {
         return
     }
 
-    let newAvailableBalance = totalBalence - withdrowAmmount;
-
-    document.getElementById("total-balence").innerText = newAvailableBalance;
+    let newAvailableBalance = availableBalance - withdrowAmmount;
+    document.getElementById("total-balance").innerText = newAvailableBalance;
 })
