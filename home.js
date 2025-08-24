599ca2fa752738a53d39b78897190cb8ac262c67
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-08-24 19:25:59 +0600

    all update

--- home.js
+++ home.js
@@ -1,4 +1,7 @@
-
+// ......... univarsal varialble ..........
+const fixedaccountNumber = 12345678910;
+const fixedPin = 2004;
+const transactionsData = [];
 // .......Reuseable function ........
 // ............. get innet text ..........
 function getInnerText(id) {
@@ -26,32 +29,113 @@ function getInputIntValue(id) {
     return IntValue
 }
 
+// -------- get Btn toggle feature -----------
+function btnToggle(id, section) {
+    const Sections = document.getElementsByClassName(section);
+    const btn = document.getElementsByClassName(id);
+    for (const form of Sections) {
+        form.style.display = "none";
+    }
+    document.getElementById(id).style.display = "block";
 
-// -------univarsal varialble ------
-const fixedaccountNumber = 12345678910;
-const fixedPin = 2004;
+}
 
+//  .................... all card  btn toggle  .................
 
-//  ----------- all card  btn toggle  --------
-const addMoneySection = document.getElementById("add-money-status");
-const cashOutSection = document.getElementById("cash-out-status");
-const addMoneyBtn = document.getElementById("add-money-card-btn");
-const cashOutBtn = document.getElementById("cash-out-btn");
-
-addMoneyBtn.addEventListener("click", function (e) {
+// ------ add moeny card btn features -------
+document.getElementById("add-money-card-btn").addEventListener("click", function (e) {
     e.preventDefault()
-    cashOutSection.style.display = 'none'
-    addMoneySection.style.display = "block";
+    btnToggle("add-money-section", "section")
+})
+
+// ------ cash out  card btn features-------
+document.getElementById("cash-out-card-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    btnToggle("cash-out-section", "section")
+})
+
+// ------ transfor-card-btn features -------
+document.getElementById("transfor-card-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    btnToggle("transfer-section", "section")
 
 })
 
-cashOutBtn.addEventListener("click", function (e) {
+// ------ Get Bonus -card-btn features -------
+document.getElementById("get-bonus-card-btn").addEventListener("click", function (e) {
     e.preventDefault()
-    addMoneySection.style.display = "none";
-    cashOutSection.style.display = 'block'
+    btnToggle("get-bonus-section", "section")
+
 })
 
-// ...... feature button ........
+// ------ Pay Bill Card Btn  features -------
+document.getElementById("pay-bill-card-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    btnToggle("pay-bil-section", "section")
+})
+
+// ------ Transactions Card Btn  features -------
+document.getElementById("transactions-card-btn").addEventListener("click", function (e) {
+    e.preventDefault()
+    btnToggle("transactions-section", "section")
+})
+
+document.getElementById("transactions-card-btn").addEventListener(
+    "click", function () {
+        const transactionsContainer = document.getElementById("transaction-container");
+        transactionsContainer.innerText =""
+        for (const data of transactionsData) {
+            const div = document.createElement("div");
+            div.innerHTML = `
+            <div class="cards w-sm flex justify-between items-center my-3 mx-auto p-3 bg-white rounded-xl">
+                <div class="flex gap-5">
+                    <div class="bg-[#f4f4f4] rounded-full p-3"><img src="./assets/purse1.png" alt=""></div>
+                    <div class="tittle">
+                        <h1 class=" text-lg font-semibold">${data.name}</h1>
+                        <p class="text-sm">${data.time}</p>
+                    </div>
+                </div>
+                <div class="icons"><i class="fa-solid fa-ellipsis-vertical"></i></div>
+            </div>
+            `
+
+            transactionsContainer.appendChild(div)
+        }
+        console.log(transactionsData)
+
+    }
+)
+
+/**
+
+
+// --------------leatest history -------------
+function(){
+        const leatestContainer = document.getElementById("leatest-container");
+        leatestContainer.innerText =""
+        for (const data of transactionsData) {
+            const div = document.createElement("div");
+            div.innerHTML = `
+            <div class="cards w-sm flex justify-between items-center my-3 mx-auto p-3 bg-white rounded-xl">
+                <div class="flex gap-5">
+                    <div class="bg-[#f4f4f4] rounded-full p-3"><img src="./assets/purse1.png" alt=""></div>
+                    <div class="tittle">
+                        <h1 class=" text-lg font-semibold">${data.name}</h1>
+                        <p class="text-sm">${data.time}</p>
+                    </div>
+                </div>
+                <div class="icons"><i class="fa-solid fa-ellipsis-vertical"></i></div>
+            </div>
+            `
+
+            leatestContainer.appendChild(div)
+        }
+        // console.log(leatestContainer)
+}
+ *
+ */
+
+// ....................... feature button .................
 // -----add money button ---
 document.getElementById("add-money-btn").addEventListener("click", function (e) {
     e.preventDefault()
@@ -72,10 +156,16 @@ document.getElementById("add-money-btn").addEventListener("click", function (e) 
 
     const newAvailableBalance = availableBalance + addAmount;
     document.getElementById("total-balance").innerText = newAvailableBalance
+
+    const data = {
+        name: "Add Money",
+        time: new Date().toLocaleTimeString()
+    }
+    transactionsData.push(data)
+    console.log(transactionsData)
 })
 // ............ withdrow btn features .........
-const withdrowBtn = document.getElementById("withdrow-money-btn")
-withdrowBtn.addEventListener("click", function (e) {
+document.getElementById("withdrow-money-btn").addEventListener("click", function (e) {
     e.preventDefault()
     const availableBalance = getInnerText("total-balance");
     const agentNumber = getInputIntValue("agent-number");
@@ -93,4 +183,11 @@ withdrowBtn.addEventListener("click", function (e) {
 
     let newAvailableBalance = availableBalance - withdrowAmmount;
     document.getElementById("total-balance").innerText = newAvailableBalance;
+
+    const data = {
+        name: "Cash Out",
+        time: new Date().toLocaleTimeString()
+    }
+    transactionsData.push(data)
+    console.log(transactionsData)
 })
